aa7bb58c735547f4459d25865035fd73c7a94a6d
rudolif <rudolifrudolif@gmail.com>
2019-08-30 14:18:20 +0200

    training commit

--- gulpfile.js
+++ gulpfile.js
@@ -50,17 +50,6 @@ gulp.task('styles', function() {
 
 gulp.task('minjs', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',  			//----jquery
-		'app/libs/jquery.validate.js', 					//----форма
-		'app/libs/jquery.mask.min.js', 					//----форма
-		'app/libs/jquery.popupoverlay.js', 				//----модалки
-		// 'app/libs/slick/slick.js', 					//----слайдер
-		 // 'app/libs/flipclock/flipclock.js',
-		// 'app/libs/swiper/swiper.min.js', 			//----слайдер
-		// 'app/libs/fancybox/jquery.fancybox.js', 		//----картінка прикліку
-		// 'app/libs/jquery.spincrement.min.js', 		//----цифри анімованні
-		// 'app/libs/isotope.pkgd.min.js', 				//----сетка елементов + фильтр
-		'app/libs/lazy-line-painter-1.9.3.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(plumber())
@@ -73,17 +62,6 @@ gulp.task('minjs', function() {
 
 gulp.task('js', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',  			//----jquery
-		'app/libs/jquery.validate.js', 					//----форма
-		'app/libs/jquery.mask.min.js', 					//----форма
-		'app/libs/jquery.popupoverlay.js', 				//----модалки
-		// 'app/libs/slick/slick.js', 					//----слайдер
-		 // 'app/libs/flipclock/flipclock.js',			//----таймер
-		// 'app/libs/swiper/swiper.min.js', 			//----слайдер
-		// 'app/libs/fancybox/jquery.fancybox.js', 		//----картінка прикліку
-		// 'app/libs/jquery.spincrement.min.js', 		//----цифри анімованні
-		// 'app/libs/isotope.pkgd.min.js', 				//----сетка елементов + фильтр
-		'app/libs/lazy-line-painter-1.9.3.min.js',
 		'app/js/common.js', // Always at the end
 	])
 	.pipe(plumber())
